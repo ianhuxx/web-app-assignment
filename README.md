2356418f27ff751d5b5b9f9f47dc659f40e967fd
ianhuxx <yh2931@nyu.edu>
2021-12-15 02:10:16 -0500

    commiteeeeeeeeee

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Flask-MongoDB Web App
+## Flask-MongoDB Team Signup Site
+### Introduction
+This a functions as a class website. It has two main features: signup and discussion. On the signup sheet students can create their own teams, join others teams or view existing teams' information. On the Discussion board students can share their through or ideas and respond to others' ideas. The edit and delete function are also available for discussion board.
 
-In this assignment you will create a web app that relies upon a MongoDB database.
-
-Replace the contents of this file with a description of your own web app, as described in [the instructions](./instructions.md).
+### Website
+This website is written and teseted using MongoDB Atlas as database server. The online version faced tremendous problem with the NYU server crashing constantly. Please set up website for local testeing and usage.
--- app.py
+++ app.py
@@ -8,6 +8,19 @@ from bson.objectid import ObjectId
 import os
 import subprocess
 
+
+client = pymongo.MongoClient("mongodb+srv://yh2931:Huyb010424@cluster0.qutr9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.test
+
+'''
+mongo = pymongo.MongoClient(
+    host = "localhost",
+    port = 27018,
+    SERVER_SELECTION_TIMEOUT=1000
+    )
+
+db = mongo.assignment
+'''
 # instantiate the app
 app = Flask(__name__)
 
@@ -21,12 +34,14 @@ if config['FLASK_ENV'] == 'development':
     # turn on debugging, if in development
     app.debug = True # debug mnode
 
+'''
 # make one persistent connection to the database
 connection = pymongo.MongoClient(config['MONGO_HOST'], 27017, 
                                 username=config['MONGO_USER'],
                                 password=config['MONGO_PASSWORD'],
                                 authSource=config['MONGO_DBNAME'])
 db = connection[config['MONGO_DBNAME']] # store a reference to the database
+'''
 
 # set up the routes
 
@@ -35,7 +50,11 @@ def home():
     """
     Route for the home page
     """
-    return render_template('index.html')
+    teams = db.teams
+    discussions = db.discussions
+    most_recent_team = {db.teams.find({"_id": 0, "created_at": 1}).limit(1)}
+    most_recent_discussion = {db.teams.find({"_id": 0, "created_at": 1}).limit(1)}
+    return render_template('index.html', teams=teams, discussions=discussions, most_recent_team=most_recent_team, most_recent_discussion=most_recent_discussion)
 
 
 @app.route('/read')
@@ -44,9 +63,19 @@ def read():
     Route for GET requests to the read page.
     Displays some information for the user with links to other pages.
     """
-    docs = db.exampleapp.find({}).sort("created_at", -1) # sort in descending order of created_at timestamp
+     # sort in descending order of created_at timestamp
+    docs = db.discussions.find({}).sort("created_at", -1)
     return render_template('read.html', docs=docs) # render the read template
 
+@app.route('/view_team')
+def view_team():
+    """
+    Route for GET requests to the read page.
+    Displays some information for the user with links to other pages.
+    """
+     # sort in descending order of created_at timestamp
+    docs = db.teams.find({}).sort("created_at", -1)
+    return render_template('view_team.html', docs=docs) # render the read template
 
 @app.route('/create')
 def create():
@@ -73,10 +102,71 @@ def create_post():
         "message": message, 
         "created_at": datetime.datetime.utcnow()
     }
-    db.exampleapp.insert_one(doc) # insert a new document
+    db.discussions.insert_one(doc) # insert a new document
 
     return redirect(url_for('read')) # tell the browser to make a request for the /read route
 
+@app.route('/create_team')
+def create_team_1():
+    """
+    Route for GET requests to the create page.
+    Displays a form users can fill out to create a new document.
+    """
+    return render_template('create_team.html') # render the create template
+
+
+@app.route('/create_team', methods=['POST'])
+def create_team():
+    """
+    Route for POST requests to the create page.
+    Accepts the form submission data for a new document and saves the document to the database.
+    """
+    name = request.form['fname']
+    email = request.form['femail']
+    project_title = request.form['fproj_title']
+    description = request.form['fdescription']
+
+    # create a new document with the data the user entered
+    doc = {
+        "name": name,
+        "email": email,
+        "project_title": project_title,
+        "description": description,
+        "created_at": datetime.datetime.utcnow(),
+        "team_members": [],
+        "team_member_count": 1
+    }
+    doc["team_members"].append({"name": name, "email": email})
+    db.teams.insert_one(doc) # insert a new document
+
+    return redirect(url_for('view_team')) # tell the browser to make a request for the /read route
+
+@app.route('/join_team')
+def join_team_1():
+    """
+    Route for GET requests to the edit page.
+    Displays a form users can fill out to edit an existing record.
+    """
+    docs = db.teams.find({}).sort("created_at", -1)
+    return render_template('join_team.html', docs = docs) # render the edit template
+
+@app.route('/join_team', methods=['POST'])
+def join_team():
+    """
+    Route for POST requests to the edit page.
+    Accepts the form submission data for the specified document and updates the document in the database.
+    """
+    name = request.form['joined_name']
+    email = request.form['joined_email']
+    project_title = request.form['team_to_join']
+
+    doc = dict(db.teams.find_one({"project_title": project_title}))
+    doc["team_members"].append({"name": name, "email": email})
+    doc["team_member_count"] = len(doc["team_members"])
+    db.teams.update({"project_title": project_title}, doc)
+
+    return redirect(url_for('view_team')) # tell the browser to make a request for the /read route
+    
 
 @app.route('/edit/<mongoid>')
 def edit(mongoid):
@@ -84,7 +174,7 @@ def edit(mongoid):
     Route for GET requests to the edit page.
     Displays a form users can fill out to edit an existing record.
     """
-    doc = db.exampleapp.find_one({"_id": ObjectId(mongoid)})
+    doc = db.discussions.find_one({"_id": ObjectId(mongoid)})
     return render_template('edit.html', mongoid=mongoid, doc=doc) # render the edit template
 
 
@@ -104,7 +194,7 @@ def edit_post(mongoid):
         "created_at": datetime.datetime.utcnow()
     }
 
-    db.exampleapp.update_one(
+    db.discussions.update_one(
         {"_id": ObjectId(mongoid)}, # match criteria
         { "$set": doc }
     )
@@ -118,7 +208,7 @@ def delete(mongoid):
     Route for GET requests to the delete page.
     Deletes the specified record from the database, and then redirects the browser to the read page.
     """
-    db.exampleapp.delete_one({"_id": ObjectId(mongoid)})
+    db.discussions.delete_one({"_id": ObjectId(mongoid)})
     return redirect(url_for('read')) # tell the web browser to make a request for the /read route.
 
 @app.route('/webhook', methods=['POST'])
